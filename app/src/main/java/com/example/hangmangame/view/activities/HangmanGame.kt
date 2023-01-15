430ae318bde0f035a92fa722fad56e5348d01dfc
Alexis Vaquero <alexisvaquero@hotmail.com>
2023-01-14 19:37:15 -0600

    finish-proyect

--- app/src/main/java/com/example/hangmangame/view/activities/HangmanGame.kt
+++ app/src/main/java/com/example/hangmangame/view/activities/HangmanGame.kt
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.children
+import com.example.hangmangame.R
 import com.example.hangmangame.databinding.ActivityHangmanGameBinding
 import com.example.hangmangame.model.HangmanApi
 import com.example.hangmangame.model.Words
@@ -19,17 +22,25 @@ import retrofit2.Response
 
 class HangmanGame : AppCompatActivity() {
     private lateinit var binding: ActivityHangmanGameBinding
+    private lateinit var word: String
+    private lateinit var underscoreWord: String
+    private var tries: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHangmanGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tvWinner.visibility = View.GONE
+        binding.tvLosser.visibility = View.GONE
+        binding.ivResetIcon.visibility = View.GONE
+
         //API LOGIN
         val call = Constants.getRetrofit()
             .create(HangmanApi::class.java)
             .getWords("hangman.php")
         Log.d(Constants.LOGTAG, "Request: ${call.request()}")
 
+        // Background process
         CoroutineScope(Dispatchers.IO).launch {
             call.enqueue(object : Callback<Words> {
                 override fun onResponse(call: Call<Words>, response: Response<Words>) {
@@ -38,7 +49,9 @@ class HangmanGame : AppCompatActivity() {
 
                     with(binding){
                         tvCategory.text = response.body()?.category
-                        tvWord.text = response.body()?.word
+                        word = response.body()?.word.toString()
+                        generateInitUnderscores()
+                        tvWord.text = underscoreWord
                         binding.pb.visibility = View.GONE
                     }
                 }
@@ -47,7 +60,7 @@ class HangmanGame : AppCompatActivity() {
                     binding.pb.visibility = View.GONE
                     Toast.makeText(
                         this@HangmanGame,
-                        "Error de conexión: ${t.message}",
+                        getString(R.string.error, "${t.message}"),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
@@ -56,7 +69,113 @@ class HangmanGame : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        val intent = Intent(this, HangmanGame::class.java)
-        startActivity(intent)
+
+        when(view.id){
+            R.id.ivResetIcon -> {
+                val intent = Intent(this, HangmanGame::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        binding.lettersLayout.children.forEach { letterView ->
+            if (letterView is TextView){
+                letterView.setOnClickListener {
+                    println(letterView.text.get(0))//
+                    game(letterView.text.get(0))
+
+                    letterView.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    private fun game(userLetter: Char){
+        updateWord(userLetter.lowercaseChar())
+        gameState()
+    }
+
+    fun generateInitUnderscores() {
+        val sb = StringBuilder()
+        for (x in word){
+           // Se puede modificar para en un futuro agregar dificultad
+           if (x == 'a'|| x == 'i' || x == 'u'){
+               sb.append(x)
+           } else {
+               sb.append("_")
+           }
+        }
+        binding.tvA.visibility = View.GONE
+        binding.tvI.visibility = View.GONE
+        binding.tvU.visibility = View.GONE
+        underscoreWord = sb.toString()
+    }
+
+    private fun updateWord(letterUser: Char){
+        if (word.contains(letterUser)) {
+            fillUnderscores(letterUser)
+        } else {
+            tries += 1
+            errors()
+        }
+    }
+
+    private fun fillUnderscores(letterUser: Char){
+        val sb = StringBuilder()
+
+        for (x in word){
+            if (underscoreWord.contains(x)){
+                // TRUE
+                // TA BIEN
+                sb.append(x)
+            } else {
+                // FALSE
+                if (x == letterUser) {
+                    sb.append(x)
+                } else {
+                    sb.append("_")
+                }
+            }
+        }
+        underscoreWord = sb.toString()
+        binding.tvWord.text = underscoreWord
+    }
+
+    private fun gameState(){
+        if (tries == 6){
+            // GAME OVER
+            binding.tvLosser.visibility = View.VISIBLE
+            binding.lettersLayout.visibility = View.GONE
+            binding.ivResetIcon.visibility = View.VISIBLE
+        } else if (underscoreWord == word){
+            // WIN GAME
+            binding.tvWinner.visibility = View.VISIBLE
+            binding.lettersLayout.visibility = View.GONE
+            binding.ivResetIcon.visibility = View.VISIBLE
+        }
+
+    }
+
+    private fun errors(){
+        when(tries){
+            1 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_1)
+            }
+            2 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_2)
+            }
+            3 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_3)
+            }
+            4 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_4)
+            }
+            5 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_5)
+            }
+            6 -> {
+                binding.ivHangman3.setImageResource(R.drawable.hangman_6)
+            }
+        }
     }
 }
